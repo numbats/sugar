f67a5c1ec5d0cdbcb5a55b1231060606d6d0e1b7
aarathybabu97 <abab0012@student.monash.edu>
2021-10-04 00:14:49 +1100

    function to create special class monash unit

---
+++ R/unit_info.R
@@ -0,0 +1,25 @@
+#' Create unit information
+#'
+#' Function to recieve unit information
+#'@param unit the unit code
+#'@param name string that denotes the name of the unit
+#'@param semester Integer that denotes teaching period
+#'@param year Year
+#'@param lecturer character vector with names of lecturers
+#'
+#'@return a special class, monash unit with the unit information as a list
+#'
+#'@examples
+#'unit_info(unit = "ETC5521", name = "Exploratory Data Analysis",semester=1,year=2020,
+#'lecturer = c("Emi Tanaka" = "emi.tanaka@monash.edu",  "Di Cook" = "dicook@monash.edu"))
+#'
+#' @export
+unit_info <- function(unit, name, semester,year, lecturer){
+   unitinfo <- list(unit = paste0(unit," S",semester,year), name = as.character(name), lecturer=lecturer)
+   class(unitinfo)<- "monash_unit"
+   assign("unitinfo", unitinfo, envir = .GlobalEnv)
+
+
+}
+
+
